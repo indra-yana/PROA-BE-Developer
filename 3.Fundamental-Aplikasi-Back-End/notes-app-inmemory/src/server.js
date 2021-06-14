30f234c7545280719e746d826dd80b4a20903caa
Indra Muliana <indra.ndra26@gmail.com>
2021-06-14 19:15:55 +0700

    Fix folder name

---
+++ 3.Fundamental-Aplikasi-Back-End/notes-app-inmemory/src/server.js
@@ -0,0 +1,32 @@
+const Hapi = require('@hapi/hapi');
+const notesPlugin = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesServices');
+const NotesValidator = require('./validator/notes');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    const notesService = new NotesService();
+    await server.register([
+        {
+            plugin: notesPlugin,
+            options: {
+                service: notesService,
+                validator: NotesValidator,
+            }
+        },
+    ]);
+
+    await server.start();
+    console.log(`Server running at ${server.info.uri}`);
+}
+
+init();
