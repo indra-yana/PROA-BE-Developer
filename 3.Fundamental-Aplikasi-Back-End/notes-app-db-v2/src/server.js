1108031d475d97fc92482a7fa34114dcb5ae1e9d
Indra Muliana <indra.ndra26@gmail.com>
2021-06-22 21:22:18 +0700

    user registration implemented

--- 3.Fundamental-Aplikasi-Back-End/notes-app-db-v2/src/server.js
+++ 3.Fundamental-Aplikasi-Back-End/notes-app-db-v2/src/server.js
@@ -1,9 +1,16 @@
 const dotenv = require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+// Notes Plugin
 const notesPlugin = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+
+// Users Plugin
+const usersPlugin = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async() => {
     const server = Hapi.server({
@@ -17,13 +24,23 @@ const init = async() => {
     });
 
     const notesService = new NotesService();
-    await server.register([{
-        plugin: notesPlugin,
-        options: {
-            service: notesService,
-            validator: NotesValidator,
-        }
-    }, ]);
+    const usersService = new UsersService();
+    await server.register([
+        {
+            plugin: notesPlugin,
+            options: {
+                service: notesService,
+                validator: NotesValidator,
+            }
+        },
+        {
+            plugin: usersPlugin,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            }
+        }, 
+    ]);
 
     await server.start();
     console.log(`Server running at ${server.info.uri}`);
