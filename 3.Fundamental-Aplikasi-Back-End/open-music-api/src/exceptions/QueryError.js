5a9df29ea4ab99c6b843c12aeb9ed2177b0a7937
Indra Muliana <indra.ndra26@gmail.com>
2021-06-26 20:17:57 +0700

    Improving Query Error Exception

--- 3.Fundamental-Aplikasi-Back-End/open-music-api/src/exceptions/QueryError.js
+++ 3.Fundamental-Aplikasi-Back-End/open-music-api/src/exceptions/QueryError.js
@@ -1,10 +1,19 @@
 const ServerError = require("./ServerError");
 
 class QueryError extends ServerError {
-    
-    constructor(message = 'Terjadi kegagalan query') {
+
+    constructor({ message = 'Terjadi kegagalan query', error = null }) {
         super(message);
         this.name = 'QueryError';
+        this._error = error;
+
+        this.saveLog();
+    }
+
+    // In case you will save error log into database
+    saveLog() {
+        console.log(this.message);
+        console.log(this._error.message);
     }
 
 }
--- 3.Fundamental-Aplikasi-Back-End/open-music-api/src/services/postgres/SongsService.js
+++ 3.Fundamental-Aplikasi-Back-End/open-music-api/src/services/postgres/SongsService.js
@@ -18,7 +18,11 @@ class SongsService {
         const result = await this._pool.query({
             text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $7) RETURNING id',
             values: [id, title, year, performer, genre, duration, insertedAt],
-        });
+        }).catch(error => ({ error }));
+
+        if (result.error) {
+            throw new QueryError({error: result.error});
+        }
 
         if (!result.rows[0].id) {
             throw new InvariantError('Lagu gagal ditambahkan');
@@ -31,7 +35,7 @@ class SongsService {
         const result = await this._pool.query('SELECT id, title, performer FROM songs LIMIT 1').catch(error => ({ error }));
 
         if (result.error) {
-            throw new QueryError();
+            throw new QueryError({error: result.error});
         }
 
         return result.rows.map(mapSongsDBToModel2);
@@ -41,7 +45,11 @@ class SongsService {
         const result = await this._pool.query({
             text: 'SELECT * FROM songs WHERE id = $1',
             values: [id],
-        });
+        }).catch(error => ({ error }));
+
+        if (result.error) {
+            throw new QueryError({error: result.error});
+        }
 
         if (!result.rowCount) {
             throw new NotFoundError('Lagu tidak ditemukan');
@@ -55,7 +63,11 @@ class SongsService {
         const result = await this._pool.query({
             text: 'UPDATE songs SET title = $1, year = $2, performer = $3, genre = $4, duration = $5, updated_at = $6 WHERE id = $7 RETURNING id',
             values: [title, year, performer, genre, duration, updatedAt, id],
-        });
+        }).catch(error => ({ error }));
+
+        if (result.error) {
+            throw new QueryError({error: result.error});
+        }
 
         if (!result.rowCount) {
             throw new NotFoundError('Gagal memperbarui lagu. Id tidak ditemukan');
@@ -66,7 +78,11 @@ class SongsService {
         const result = await this._pool.query({
             text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
             values: [id],
-        });
+        }).catch(error => ({ error }));
+
+        if (result.error) {
+            throw new QueryError({error: result.error});
+        }
 
         if (!result.rowCount) {
             throw new NotFoundError('Lagu gagal dihapus, Id tidak ditemukan');
